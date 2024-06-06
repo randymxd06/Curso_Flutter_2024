5b1b9dd5037fc8e542eb93cc74d33a19f1df1c92
randymxd06 <randym0624@gmail.com>
2024-06-05 20:39:43 -0400

    feat: :sparkles: Add Widgets App continuation section
    In this section I saw progress, dialogs, licenses, tools, tutorial and how to make an infinitescroll

---
+++ 08 - Widgets App (Continuacion)/lib/config/menu/menu_items.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon
+  });
+}
+
+
+const appMenuItems = <MenuItem>[
+
+  MenuItem(
+    title: 'Botones', 
+    subTitle: 'Varios botones en Flutter', 
+    link: '/buttons', 
+    icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+    title: 'Tarjetas', 
+    subTitle: 'Un contenedor estilizado', 
+    link: '/cards', 
+    icon: Icons.credit_card
+  ),
+
+  MenuItem(
+    title: 'Progress Indicators', 
+    subTitle: 'Generales y controlados', 
+    link: '/progress', 
+    icon: Icons.refresh_rounded
+  ),
+
+  MenuItem(
+    title: 'Snackbars y diálogos', 
+    subTitle: 'Indicadores en pantalla', 
+    link: '/snackbars', 
+    icon: Icons.info_outline
+  ),
+
+  MenuItem(
+    title: 'Animated container', 
+    subTitle: 'Stateful widget animado', 
+    link: '/animated', 
+    icon: Icons.check_box_outline_blank_rounded
+  ),
+
+  MenuItem(
+    title: 'UI Controls + Tiles', 
+    subTitle: 'Una serie de controles de Flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_outlined
+  ),
+
+  MenuItem(
+    title: 'Introducción a la aplicación', 
+    subTitle: 'Pequeño tutorial introductorio', 
+    link: '/tutorial', 
+    icon: Icons.accessible_rounded
+  ),
+
+  MenuItem(
+    title: 'InfiniteScroll y Pull', 
+    subTitle: 'Listas infinitas y pull to refresh', 
+    link: '/infinite', 
+    icon: Icons.list_alt_rounded
+  ),
+
+
+];
+
+
+
